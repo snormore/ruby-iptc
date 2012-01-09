9e0e45689deb509bddd4dd7efde92c7fe78088a0
Pierre Baillet <oct@fotonauts.com>
2012-01-09 15:12:53 +0100

    Reindent, add new test case

--- test/image_simple/load_test.rb
+++ test/image_simple/load_test.rb
@@ -3,18 +3,24 @@ $: << "../../lib"
 require 'iptc'
 
 class Image_Test < Test::Unit::TestCase
-	def test_file_loading
-		folder_name = File.dirname(__FILE__)
-		i = IPTC::JPEG::Image.from_file( File.join(folder_name, 'flickr-2662825452-original.jpg' ) )
+  def test_file_loading
+    folder_name = File.dirname(__FILE__)
+    i = IPTC::JPEG::Image.from_file( File.join(folder_name, 'flickr-2662825452-original.jpg' ) )
     assert_equal(58, IPTC::MarkerNomenclature.instance.markers_count)
-	end
+  end
   def test_blob_loading
     folder_name = File.dirname(__FILE__)
     data = File.open( File.join(folder_name, 'flickr-2662825452-original.jpg' )).binmode.read
     i = IPTC::JPEG::Image.from_blob( data )
     assert_equal(58, IPTC::MarkerNomenclature.instance.markers_count)
   end
+end
 
+class Image2_Test < Test::Unit::TestCase
+  def test_file_loading
+    folder_name = File.dirname(__FILE__)
+    i = IPTC::JPEG::Image.from_file( File.join(folder_name, 'from-Ludovic-Peron.jpg' ) )
+  end
 end
 
 
